ee23342747848f8d841e9fcffb8f17dac4c0253e
Adem Kouki <ademkingnew@gmail.com>
2020-06-17 14:35:56 +0100

    Fix Bug

--- app.py
+++ app.py
@@ -2,22 +2,6 @@ from trello import TrelloAPI as Trello
 from openpyxl import load_workbook
 
 
-def TrelloImport(api_key, token, board_id, list_name, xlsx_file_path, label_name):
-    MyTrello = Trello(api_key, token)
-    B = MyTrello.GetBoardById(board_id)
-    List = B.GetListByName(list_name)
-    wb = load_workbook(xlsx_file_path)
-    sheet = wb.active
-    max_row = sheet.max_row
-    label_name = B.GetLabelByName(label_name)
-    for i in range(1, max_row+1):
-
-        card_content = sheet.cell(row=i, column=1).value
-        label = sheet.cell(row=i, column=2).value
-        Card = MyTrello.new('Card', card_content, List)
-        Card.AddLabel(label_name)
-
-
 def main():
     api_key = "YOUR_API_KEY_HERE"
     token = "YOUR_TOKEN_HERE"
@@ -25,8 +9,27 @@ def main():
     list_name = "LIST_NAME_HERE"
     xlsx_file_path = "data.xlsx"
     label_name = "LABEL_NAME_HERE"
-    TrelloImport(api_key, token, board_id, list_name,
-                 xlsx_file_path, label_name)
+
+    MyTrello = Trello(api_key,token)
+    B = MyTrello.GetBoardById(board_id)
+    TodoList = B.GetListByName(list_name)
+    wb = load_workbook(xlsx_file_path)
+    sheet = wb.active
+    max_row = sheet.max_row
+    label = B.GetLabelByName(label_name)
+    for i in range(2, max_row+1):
+        # iterate over all columns
+        print(i)
+        question = sheet.cell(row=i, column=1).value
+        Card = MyTrello.new('Card', question, TodoList)
+        comment = sheet.cell(row=i, column=2).value
+        # Test if comment is empty
+        if comment is not None:
+            Card.SetDesc(comment)
+        priority = sheet.cell(row=i, column=3).value
+        difficulty = sheet.cell(row=i, column=4).value
+        Card.AddLabel(label)
+        
 
 if __name__ == '__main__':
     main()
